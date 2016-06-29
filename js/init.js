74fd5fc3d091f75a56f9341cfec881a9ca08445d
pwang347 <paul_wang347@hotmail.com>
2016-06-29 14:20:10 -0700

    small update to default.html to preserve original mobile view

--- js/init.js
+++ js/init.js
@@ -18,7 +18,15 @@
 
 	skel.init({
 		reset: 'full',
-		containers: '100%'
+		containers: '100%',
+		breakpoints: {
+			global: { href: '/css/style.css', grid: { gutters: ['2.5em', 0] } },
+			xlarge: { media: '(max-width: 1800px)', href: '/css/style-xlarge.css' },
+			large: { media: '(max-width: 1280px)', href: '/css/style-large.css', grid: { gutters: ['2em', 0] } },
+			medium: { media: '(max-width: 980px)', href: '/css/style-medium.css'},
+			small: { media: '(max-width: 736px)', href: '/css/style-small.css', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
+			xsmall: { media: '(max-width: 480px)', href: '/css/style-xsmall.css' }
+		}
 	});
 
 	$(function() {
